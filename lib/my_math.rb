3040243f27f8cf31371d90977979c41c7fb0608c
Frank West <frank.west@phishme.com>
2015-11-12 15:04:08 -0800

    moved to point struct

--- lib/my_math.rb
+++ lib/my_math.rb
@@ -1,34 +1,34 @@
 class MyMath
-  attr_reader :x, :y
+  Point = Struct.new(:x, :y)
+  attr_reader :point
 
   def initialize(x, y)
-    @x = x
-    @y = y
+    @point = Point.new(x, y)
   end
 
   def complicated_math_equation
     (
-      (0.06/x) /x ) *
+      (0.06/point.x) /point.x ) *
       (
         (
-          Math.log(1+(14000/200)*(1-(0.06)**y))
+          Math.log(1+(14000/200)*(1-(0.06)**point.y))
     ) /
       Math.log(1+0.06))
   end
 
   def add
-    x + y
+    point.x + point.y
   end
 
   def subtract
-    x - y
+    point.x - point.y
   end
 
   def multiply
-    x * y
+    point.x * point.y
   end
 
   def divide
-    x / y
+    point.x / point.y
   end
 end
