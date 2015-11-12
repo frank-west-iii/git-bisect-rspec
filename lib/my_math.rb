89071f5c1602bcb1f9b7e898c953aa21f9f63896
Frank West <frank.west@phishme.com>
2015-11-12 14:50:24 -0800

    add method implemented

--- lib/my_math.rb
+++ lib/my_math.rb
@@ -2,4 +2,8 @@ class MyMath
   def self.complicated_math_equation(x, y)
     ((0.06/x)/y) * ((Math.log(1+(14000/200)*(1-(0.06)**y)))/Math.log(1+0.06))
   end
+
+  def self.add(x, y)
+    x + y
+  end
 end
--- spec/my_math_spec.rb
+++ spec/my_math_spec.rb
@@ -9,4 +9,10 @@ describe MyMath do
       expect(subject.complicated_math_equation(365, 30)).to eq(0.0004008509986404663)
     end
   end
+
+  describe '#add' do
+    it 'add the numbers' do
+      expect(subject.add(365, 30)).to eq(395)
+    end
+  end
 end
